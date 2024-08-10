6cb33b03cf55c9ae4f92aa4e782861d7841a9a32
Xinbang Hu <139057795+yuanair@users.noreply.github.com>
2024-08-10 15:53:30 +0800

    TFStaticArray

---
+++ Whale/Core/Whale/Container/TFStaticArray.cpp
@@ -0,0 +1,9 @@
+//
+// Created by admin on 2024/8/10.
+//
+
+#include "TFStaticArray.hpp"
+
+namespace Whale
+{
+} // Whale
---
+++ Whale/Core/Whale/Container/TFStaticArray.hpp
@@ -0,0 +1,68 @@
+//
+// Created by admin on 2024/8/10.
+//
+
+#pragma once
+
+#include "../Utility.hpp"
+
+namespace Whale
+{
+	
+	/// 静态数组
+	template<class ElemT, SizeT TheLength>
+	class WHALE_API TFStaticArray
+	{
+	public:
+		
+		using ElemType = ElemT;
+	
+	public:
+		
+		static constexpr SizeT m_length = TheLength;
+	
+	public:
+		
+		TFStaticArray();
+		
+		TFStaticArray(const TFStaticArray &other);
+		
+		TFStaticArray(TFStaticArray &&other);
+		
+		TFStaticArray(const std::initializer_list<ElemT> &initializerList);
+		
+		~TFStaticArray() noexcept;
+	
+	public:
+		
+		TFStaticArray &operator=(const TFStaticArray &other);
+		
+		TFStaticArray &operator=(TFStaticArray &&other);
+		
+		ElemT &operator[](SizeT index);
+		
+		const ElemT &operator[](SizeT index) const;
+	
+	public:
+		
+		ElemT &At(SizeT index);
+		
+		const ElemT &At(SizeT index) const;
+		
+		ElemT &AtBack(SizeT index);
+		
+		const ElemT &AtBack(SizeT index) const;
+		
+		void Swap(TFStaticArray &other);
+	
+	public:
+		
+		[[nodiscard]] SizeT GetLength() const { return m_length; }
+	
+	private:
+		
+		ElemT m_arr[TheLength];
+		
+	};
+	
+} // Whale
